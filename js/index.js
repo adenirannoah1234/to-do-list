ef83adad4add1887302fcf2a2ee8e197271adc07
adenirannoah1234 <adenirannoah9@gmail.com>
2023-08-28 11:21:44 +0100

    initial changes

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const toDo = document.querySelector('#listing');
+const submitBtn = document.querySelector('.submit')
+const alert = document.querySelector('.alert');
+const paraGraph = document.querySelector('.paragraph');
+
+submitBtn.addEventListener('click', function (e) {
+    e.preventDefault()
+    if (toDo.value === '') {
+        alert.style.display = 'block';
+        alert.textContent = 'Please Fill in the fields!';
+        setTimeout(function () {
+            alert.style.display = 'none';
+        }, 5000)
+    } else {
+        paraGraph.innerHTML += `<p>${toDo.value}</p>`
+    }
+})
