1fde5db35e9284edfe55d30a75d70c56282d45c6
adenirannoah1234 <adenirannoah9@gmail.com>
2023-08-30 11:18:09 +0100

    initial changes

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@ const toDo = document.querySelector('#listing');
 const submitBtn = document.querySelector('.submit')
 const alert = document.querySelector('.alert');
 const paraGraph = document.querySelector('.paragraph');
+document.querySelector('.nav').innerHTML = `<nav>
+<a href="/">TODO</a>
+<a href="#">ABOUT</a>
+</nav>
+`;
+
 
 submitBtn.addEventListener('click', function (e) {
     e.preventDefault()
@@ -13,6 +19,7 @@ submitBtn.addEventListener('click', function (e) {
         }, 5000)
     } else {
         paraGraph.innerHTML += `<p>${toDo.value}</p>`
+        toDo.value = '';
     }
 })
 
@@ -21,9 +28,8 @@ function updateDate() {
     const year = now.getFullYear();
     const month = now.getMonth() + 1; // Months are zero-based
     const day = now.getDate();
-    
-    // Format the date as yyyy-mm-dd
-    const formattedDate = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+
+    const formattedDate = `${year} - ${month} - ${day}`;
     
     // Update the content of the date element
     const dateElement = document.getElementById('date');
@@ -33,7 +39,14 @@ function updateDate() {
   // Update the date immediately and then set an interval to update it daily
   updateDate();
   setInterval(updateDate, 86400000); // Update every 86400000ms (24 hours)
-    
+  
 
 
 // tip calculator media queries, object and function.
+
+// local storage
+
+localStorage.setItem('username', 'Noah');
+localStorage.setItem('AGE', '25');
+const x = localStorage.getItem('username')
+document.querySelector('.localstorage').textContent = x
